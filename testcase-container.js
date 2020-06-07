8e06a9707cbf90789161af305187e5d358e8a541
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-07 12:47:09 -0400

    pm buildout

--- testcase-container.js
+++ testcase-container.js
@@ -177,7 +177,7 @@ class PMTestCaseContainer extends Component {
 				id = item.id;
 			}
 			fuLogger.log({level:'TRACE',loc:'TestCaseContainer::onModify',msg:"test"+id});
-			this.props.actions.modifyItem(id);
+			this.props.actions.modifyItem({id,appPrefs:this.props.appPrefs});
 		};
 	}
 	
